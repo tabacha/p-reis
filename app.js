4c4a13070a7fd080c56cbdb9997767054235bd62
Sven Anders <github-commit@sven.anders.im>
2015-02-24 09:09:38 +0100

    Move functionalitity to extra files, extended calls

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
-    passport = require('passport'),
+    passport = require('./lib/passport-reis'),
     i18n = require('i18n'),
     flash = require('connect-flash'),
     session = require('express-session'),
-    LocalStrategy = require('passport-local').Strategy,
+    callsRouter = require('./lib/calls'),
     PORT_LISTENER = 3000;
-console.log('I am listening to this port: http://localhost:%s', PORT_LISTENER);
+
+
 
 
 i18n.configure({
@@ -21,57 +22,6 @@ function ensureAuthenticated(req, res, next) {
   res.redirect('/login')
 }
 
-var users = [
-    { id: 1, username: 'bob', password: 'secret', email: 'bob@example.com' }
-  , { id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com' }
-];
-
-function findById(id, fn) {
-  var idx = id - 1;
-  if (users[idx]) {
-    fn(null, users[idx]);
-  } else {
-    fn(new Error('User ' + id + ' does not exist'));
-  }
-}
-
-function findByUsername(username, fn) {
-  for (var i = 0, len = users.length; i < len; i++) {
-    var user = users[i];
-    if (user.username === username) {
-      return fn(null, user);
-    }
-  }
-  return fn(null, null);
-}
-
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-  findById(id, function (err, user) {
-    done(err, user);
-  });
-});
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    // asynchronous verification, for effect...
-    process.nextTick(function () {
-      
-      // Find the user by username.  If there is no user with the given
-      // username, or the password is not correct, set the user to `false` to
-      // indicate failure and set a flash message.  Otherwise, return the
-      // authenticated `user`.
-      findByUsername(username, function(err, user) {
-        if (err) { return done(err); }
-          if (!user) { return done(null, false, { message: '1'});} 
-          if (user.password != password) { return done(null, false, { message: '2' });}; 
-        return done(null, user);
-      })
-    });
-  }
-));
 
 var app = express();
 
@@ -107,14 +57,6 @@ app.get('/welcome', ensureAuthenticated, function(req, res) {
 app.get('/',  function(req, res) {
   res.redirect('/calls');
 });
-app.get('/calls', ensureAuthenticated, function(req, res) {
-    res.render('calls', {
-        menu: 'calls',
-        title: req.__('Calls'),
-        user:req.user,
-    });
-
-});
 app.get('/login', function(req, res) {
     msg="";
     err=req.flash('error');
@@ -145,10 +87,13 @@ app.get('/logout', function(req, res){
         user:req.user,
   });
 });
-
 express.static.mime.define({'text/css': ['css']});
 app.use('/css',
         express.static(__dirname + '/css'));
 app.use('/components',
         express.static(__dirname + '/bower_components'));
+// ----------------------------------------------------------------------------
+app.all('*', ensureAuthenticated);
+app.use('/calls',callsRouter);
+console.log('I am listening to this port: http://localhost:%s', PORT_LISTENER);
 app.listen(PORT_LISTENER);
